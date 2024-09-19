c9a20b98346d4a39bbd7167979b043f8f0b251e5
angelmari0405 <angelmari0405@gmail.com>
2024-09-19 10:08:16 -0500

    first commit frontend

---
+++ src/apis/authentication.js
@@ -0,0 +1,81 @@
+import { DOMAIN } from './config'
+
+export const registerApi = async (bodyObject) => {    
+    
+    const requestOptions = {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(bodyObject) 
+    };
+   
+    try 
+    {
+        const response = await fetch(`${DOMAIN}/users`, requestOptions);
+        if(response.ok) 
+        {
+            const result = await response.json();
+            return [result,'']
+        }      
+        const errorMessage = await response.text();      
+        return ['',`Server side error: ${errorMessage}`];
+    } catch (error) 
+    {
+        return ['',`Server down: ${error}`]
+    }
+
+}
+
+export const loginApi = async (bodyObject) => {    
+    
+    const requestOptions = {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(bodyObject) 
+    };
+   
+    try 
+    {
+        const response = await fetch(`${DOMAIN}/users/sign_in`, requestOptions);
+        if(response.ok) 
+        {
+            const result = await response.json();
+            return [result,'']
+        }
+        const errorMessage = await response.text();      
+        return ['',`Server side error: ${errorMessage}`];
+    } catch (error) 
+    {
+        return ['',`Server down: ${error}`]
+    }
+
+}
+export const loginoutApi = async (bodyObject) => {    
+    
+    const requestOptions = {
+    method: 'DELETE',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(bodyObject) 
+    };
+   
+    try 
+    {
+        const response = await fetch(`${DOMAIN}/users/sign_out`, requestOptions);
+        if(response.ok) 
+        {
+            const result = await response.json();
+            return [result,'']
+        }
+        const errorMessage = await response.text();      
+        return ['',`Server side error: ${errorMessage}`];
+    } catch (error) 
+    {
+        return ['',`Server down: ${error}`]
+    }
+
+}
